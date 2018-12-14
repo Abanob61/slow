d95bc5e81a2e3eb4481dee13f8009ceff79eee93
Abanob <abanobgerges11@yahoo.com>
2018-12-14 18:43:00 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,7 +11,7 @@ async def on_ready():
     print(client.user.name)
     print(client.user.id)
     print('------')
-    await client.change_presence(game=discord.Game(name='SlowBot'), status=discord.Status("online"))
+    await client.change_presence(game=discord.Game(name='SlowBall'), status=discord.Status("online"))
 
 @client.event
 async def on_message(message):
