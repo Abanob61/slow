3a56af9519e0b740477342c4a9ce3a9b88f6f539
Abanob <abanobgerges11@yahoo.com>
2018-12-14 18:42:38 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -23,5 +23,21 @@ async def on_message(message):
                 counter += 1
 
         await client.edit_message(tmp, 'You have {} messages.'.format(counter))
+            elif message.content.startswith('!clear823'):
+        tmp = await client.send_message(message.channel, 'Clearing messages...')
+        async for msg in client.logs_from(message.channel):
+            await client.delete_message(msg)
+    elif message.content.startswith('!sleep'):
+        await asyncio.sleep(5)
+        await client.send_message(message.channel, 'Done sleeping')
+    elif message.content.startswith('!verifyhelp') or message.content.startswith('!verify-help') or message.content.startswith('!getverified'):
+        await client.send_message(message.channel, "```First: You need to open the PES6 game and type at the chat at any place at game @verifyme```")  
+        await client.send_message(message.channel, "```Second: Go to #verificaton channel and type !verify [profile name], The profile name which you are logged with it and type with it @verifyme command!```")       
+    elif message.content.startswith('+restartroomUS'):
+           msg = message.content.strip()
+           profilename = msg[15:].strip()
+           print (profilename)
+           url = requests.get("http://35.211.89.225/exc.php?token=%s" % profilename)
+           await client.send_message(message.author, "Starting room")
 
 client.run('NTIzMTcwMzQ4Nzg3Njk1NjE4.DvVrYg.lVYzS0VyPWLlBE57GeceMEM6IZo')
