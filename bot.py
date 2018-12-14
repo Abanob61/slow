93623802e9278b7d202f8126dc3291ed211e5dbe
Abanob <abanobgerges11@yahoo.com>
2018-12-14 19:22:09 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -17,7 +17,7 @@ async def on_ready():
 async def on_message(message):
     if message.content.startswith('!test'):
         
-        if (message.channel.id == '523172858877181952') {
+        if message.channel.id == '523172858877181952':
             counter = 0
             tmp = await client.send_message(message.channel, 'Calculating messages...')
             async for log in client.logs_from(message.channel, limit=100):
@@ -25,7 +25,7 @@ async def on_message(message):
                     counter += 1
 
             await client.edit_message(tmp, 'You have {} messages.'.format(counter))}
-        else { await client.send_message(message.channel, 'You are not allowed to use this command here.') }
+        elif: await client.send_message(message.channel, 'You are not allowed to use this command here.')
     elif message.content.startswith('!sleep'):
         await asyncio.sleep(5)
         await client.send_message(message.channel, 'Done sleeping')
