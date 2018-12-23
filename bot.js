7f765128dba9c51fdd356d89df8c8d7fb4f89d65
Abanob <abanobgerges11@yahoo.com>
2018-12-23 03:07:17 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,25 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+ 
+
+client.on('ready', () => {
+
+    console.log('I am ready!');
+    client.change_presence(game=discord.Game(name='SlowBall'), status=discord.Status("online"))
+
+});
+
+ 
+
+client.on('message', message => {
+
+    if (message.content === '!ping') {
+
+       message.reply('pong');
+
+       }
+
+});
+client.login(NTIzMTcwMzQ4Nzg3Njk1NjE4.DvVrYg.lVYzS0VyPWLlBE57GeceMEM6IZo);
